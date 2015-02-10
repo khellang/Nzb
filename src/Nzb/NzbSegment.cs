c0588dc0663ae65e6d1adca06a369e4aff053789
Kristian Hellang <kristian@hellang.com>
2015-02-10 02:26:42 +0100

    Added XML docs for NzbSegment

--- src/Nzb/NzbSegment.cs
+++ src/Nzb/NzbSegment.cs
@@ -13,10 +13,22 @@ namespace Nzb
             MessageId = Check.NotNull(messageId, "messageId");
         }
 
+        /// <summary>
+        /// Gets the number of bytes.
+        /// </summary>
+        /// <value>The number of bytes.</value>
         public long Bytes { get; private set; }
 
+        /// <summary>
+        /// Gets the segment number.
+        /// </summary>
+        /// <value>The segment number.</value>
         public int Number { get; private set; }
 
+        /// <summary>
+        /// Gets the Usenet message identifier.
+        /// </summary>
+        /// <value>The Usenet message identifier.</value>
         public string MessageId { get; private set; }
 
         private string DebuggerDisplay
@@ -24,6 +36,10 @@ namespace Nzb
             get { return string.Format("#{0} ({1} bytes)", Number, Bytes); }
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
         public override string ToString()
         {
             return MessageId;
