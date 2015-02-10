c39585ca7b53d177460a3fdd591ebd31a21d03e4
Kristian Hellang <kristian@hellang.com>
2015-02-10 02:24:25 +0100

    Added XML docs for NzbFile

--- src/Nzb/INzbFile.cs
+++ src/Nzb/INzbFile.cs
@@ -4,22 +4,49 @@ using JetBrains.Annotations;
 
 namespace Nzb
 {
+    /// <summary>
+    /// Represents a file linked in a NZB document.
+    /// </summary>
     public interface INzbFile : IFluentInterface
     {
+        /// <summary>
+        /// Gets the poster of the file.
+        /// </summary>
+        /// <value>The poster of the file.</value>
         [NotNull]
         string Poster { get; }
 
+        /// <summary>
+        /// Gets the date the server saw this file.
+        /// </summary>
+        /// <value>The date the server saw this file.</value>
         DateTimeOffset Date { get; }
 
+        /// <summary>
+        /// Gets the subject of the Usenet article.
+        /// </summary>
+        /// <value>The subject of the Usenet article.</value>
         [NotNull]
         string Subject { get; }
 
+        /// <summary>
+        /// Gets the groups this file has been posted in.
+        /// </summary>
+        /// <value>The groups this files has been posted in.</value>
         [NotNull, ItemNotNull]
         IReadOnlyList<string> Groups { get; }
 
+        /// <summary>
+        /// Gets the segments that makes up this file.
+        /// </summary>
+        /// <value>The segments that makes up this file.</value>
         [NotNull, ItemNotNull]
         IReadOnlyList<INzbSegment> Segments { get; }
 
+        /// <summary>
+        /// Gets the total number of bytes for all the file's segments.
+        /// </summary>
+        /// <value>The total number of bytes for all the file's segments.</value>
         long Bytes { get; }
     }
 }
--- src/Nzb/NzbFile.cs
+++ src/Nzb/NzbFile.cs
@@ -26,16 +26,40 @@ namespace Nzb
             _bytes = new Lazy<long>(() => Segments.Sum(x => x.Bytes));
         }
 
+        /// <summary>
+        /// Gets the poster of the file.
+        /// </summary>
+        /// <value>The poster of the file.</value>
         public string Poster { get; private set; }
 
+        /// <summary>
+        /// Gets the date the server saw this file.
+        /// </summary>
+        /// <value>The date the server saw this file.</value>
         public DateTimeOffset Date { get; private set; }
 
+        /// <summary>
+        /// Gets the subject of the Usenet article.
+        /// </summary>
+        /// <value>The subject of the Usenet article.</value>
         public string Subject { get; private set; }
 
+        /// <summary>
+        /// Gets the groups this file has been posted in.
+        /// </summary>
+        /// <value>The groups this files has been posted in.</value>
         public IReadOnlyList<string> Groups { get; private set; }
 
+        /// <summary>
+        /// Gets the segments that makes up this file.
+        /// </summary>
+        /// <value>The segments that makes up this file.</value>
         public IReadOnlyList<INzbSegment> Segments { get; private set; }
 
+        /// <summary>
+        /// Gets the total number of bytes for all the file's segments.
+        /// </summary>
+        /// <value>The total number of bytes for all the file's segments.</value>
         public long Bytes
         {
             get { return _bytes.Value; }
@@ -46,6 +70,10 @@ namespace Nzb
             get { return Subject; }
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
         public override string ToString()
         {
             return Subject;
