27b48140b5edfc44afb1f74f5cfe022ab4bac631
Kristian Hellang <kristian@hellang.com>
2019-01-15 19:31:31 +0100

    Use DateTimeOffset.FromUnixTimeSeconds on netstandard2.0

--- src/Nzb/EpochUtility.cs
+++ src/Nzb/EpochUtility.cs
@@ -4,10 +4,18 @@ namespace Nzb
 {
     internal static class EpochUtility
     {
+#if NETSTANDARD1_0
         private static readonly DateTimeOffset UnixEpoch =
             new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+#endif
 
-        public static DateTimeOffset ToUnixEpoch(this long timestamp) =>
-            UnixEpoch.AddSeconds(timestamp).ToLocalTime();
+        public static DateTimeOffset ToUnixEpoch(this long timestamp)
+        {
+#if NETSTANDARD1_0
+            return UnixEpoch.AddSeconds(timestamp).ToLocalTime();
+#else
+            return DateTimeOffset.FromUnixTimeSeconds(timestamp);
+#endif
+        }
     }
 }
